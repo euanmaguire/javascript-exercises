2ecec672f1c4f77404879791f9046f1caf8a5aba
Euan Maguire <euanmaguire@aol.com>
2022-07-14 23:38:08 +0100

    Done exercises 1 to 7 with succes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let list = arguments[0];
+    for(let i = 1; i < arguments.length; i++){
+        for(let j = 0; j < list.length; j++){
+            if(list[j] === arguments[i]){
+                list.splice(j,1);
+            }
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if(num1 >= 0 && num2 >= 0 && typeof num1 === "number" && typeof num2 === "number"){
+        if(num1 <= num2){
+            for(let i = num1; i <= num2; i++){
+                sum += i;
+            }
+        }
+
+        if(num1 > num2){
+            for(let i = num1; i >= num2; i--){
+                sum += i;
+            }
+        }
+    }
+    
+    else{
+        return "ERROR"
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
